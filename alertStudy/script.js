47076ed8c40e63c3475814232db4bcebcb9dca0f
PrillaTakki <mycryptoan@gmail.com>
2023-01-17 17:57:08 +0400

    newmy

--- alertStudy/script.js
+++ alertStudy/script.js
@@ -7,14 +7,14 @@ themeButton.onclick = function() {
   page.classList.toggle('dark-theme');
 };
 
-let question = prompt('Напиши число?', '');
+let question = prompt('Напиши число?', 0);
 
-if (question > 1) {
+if (question > 0) {
   alert( 1 );
 }
-  else if (question < -1) {
+  else if (question < 0) {
   alert(-1);
   }
-  else (question === 0) {
+  else  {
   alert(0);
 }
